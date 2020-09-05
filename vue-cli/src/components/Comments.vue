63784a123f62cfad88e6513ef1440ddb249452ca
Monica261 <desenvolvimento.midiasim@gmail.com>
2020-09-04 23:55:48 -0300

    inserindo a pasta vue-cli

---
+++ vue-cli/src/components/Comments.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr />
+    <FormTodo v-on:add-todo="addComment"></FormTodo>
+    <div class="list-group">
+    <p v-if="comments.length <= 0">Sem comentários...</p>
+    <div v-else class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="index">
+        <span class="comment__author"><strong>Autor: </strong>{{ comment.name }}</span>
+        <p><strong>Hamburguer: </strong>{{ comment.hamburguer }}</p>
+        <p><strong>Bebida: </strong>{{ comment.bebida }}</p>
+        <p><strong>Comentário: </strong>{{ comment.message }}</p>
+        <div>
+        <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)"><button class="btn btn-md btn-danger">Excluir</button></a>
+        </div>
+    </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+import FormTodo from './FormTodo';
+export default {
+  components: {
+    FormTodo
+  },
+  data() {
+    return {
+      comments: []
+    }
+  },
+  methods: {
+    addComment(comment) {
+      this.comments.push(comment);
+    },
+    removeComment(index) {
+      this.comments.splice(index, 1);
+    }
+  },
+  computed: {
+    allComments() {
+      return this.comments.map(comment => ({
+        ...comment,
+        name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+      }))
+    }
+  },
+  watch: {
+    comments(val) {
+      console.log('val', val)
+    }
+  }
+}
+</script>
